1c2e147d639b190e7df77171b736a4d920d715e5
abhishek <ap7785980@gmail.com>
2026-07-02 13:38:26 +0530

    Add root route and JSON middleware

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({
+        message: "Regex Validator API is running"
+    });
+});
 app.listen(3000, () => {
     console.log("Server Running...");
 });
